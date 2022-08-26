e206c799e892297163845fedf82f7d173467881b
dalvifaraz <dalvifaraz@gmail.com>
2022-08-27 00:10:15 +0530

    Components creaated and UI design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Grammar from './Component/Grammar.jsx';
+import Upload from './Component/Upload.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1>Grammarly Clone</h1>
+        <p>Grammarly Clone helps you write mistake-free in Gmail,
+          Facebook, Twitter, LinkedIn, and any other app you use.
+          Even in text messages! Write Better in Seconds.</p>
+      </div>
+
+      <Grammar />
+
+
+      <Upload />
+
+      <div id="disclaimer">Copyright © 2022 Developed by SufranKhan for Daten and Wissen Pvt. Ltd.
+      </div>
     </div>
   );
 }
